e9627ee6c5a53b7c26c5beaa6f22f6ec9ff17545
Omkar-Kamat <mail4omkarkamat@gmail.com>
2026-05-14 22:46:35 +0530

    delete frontend

--- review-reports/INDEX.md
+++
@@ -1,9 +0,0 @@
-# Lint Review Reports
-
-Generated: 2026-04-17T12:37:21.031Z
-Scanned files: 137
-Files with issues: 0
-Total issues: 0
-
-No ESLint issues were detected.
-
--- src/app/room/[roomId]/report/loading.js
+++
@@ -1,9 +0,0 @@
-import { SkeletonReport } from "@/components/ui/Skeleton";
-
-export default function ReportLoading() {
-  return (
-    <div className="min-h-screen bg-[#04040f]">
-      <SkeletonReport />
-    </div>
-  );
-}
